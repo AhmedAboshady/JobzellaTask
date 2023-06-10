1515f2862c5bd31dbe785f4ddc4f8e2ec121a979
Abo shady <a.aboshady22@gmail.com>
2023-06-10 02:04:36 +0200

    task

--- app/src/androidTest/java/com/task/jobzella/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/task/jobzella/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.task.jobzella
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,16 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule @JvmField var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.task.jobzella", appContext.packageName)
+    }
+    @Test
+    fun testButtonClick(){
+        onView(withId(R.id.btnSearch)).perform(click())
     }
 }
--- app/src/main/java/com/task/jobzella/MainActivity.kt
+++ app/src/main/java/com/task/jobzella/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.task.jobzella
 
-import android.content.pm.PackageManager
-import android.os.Build
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
+
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.get
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.task.jobzella.databinding.ActivityMainBinding
-import com.task.jobzella.viewModels.ImagesViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
+/**
+ * Get Images From Splash API And display them
+ */
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     lateinit var binding: ActivityMainBinding
